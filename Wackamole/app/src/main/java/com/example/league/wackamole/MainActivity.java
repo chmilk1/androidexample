9620a7f2f6b632a76e88db782e52bc1744fab2e3
chmilk1 <league@WTS14-3.attlocal.net>
2018-11-01 19:57:28 -0700

    1133

--- Wackamole/app/src/main/java/com/example/league/wackamole/MainActivity.java
+++ Wackamole/app/src/main/java/com/example/league/wackamole/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -15,12 +18,16 @@ public class MainActivity extends AppCompatActivity{
 
     ArrayList<Button> moles = new ArrayList<>();
     Timer posTimer = new Timer();
+    Timer gameTimer = new Timer();
     Random rand = new Random();
+    int score = 0;
+    int secondsLeft = 20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addMoles();
+        hideMoles();;
         Button start = findViewById(R.id.start);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +42,8 @@ public class MainActivity extends AppCompatActivity{
 
     private void addMoles(){
         moles.add((Button) findViewById(R.id.mole1));
+        moles.add((Button) findViewById(R.id.mole2));
+        moles.add((Button) findViewById(R.id.mole3));
     }
 
     private void addClickListner(){
@@ -43,6 +52,7 @@ public class MainActivity extends AppCompatActivity{
                 @Override
                 public void onClick(View v) {
                     changeMolePos(m);
+                    addScore();
                 }
             });
         }
@@ -62,25 +72,65 @@ public class MainActivity extends AppCompatActivity{
 
     private void startGame(){
         Button start = findViewById(R.id.start);
-        posTimer.schedule(new TimerTask() {
+        addClickListner();
+        showMoles();
+        posTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 changeMolePos();
             }
-        }, 500l);
+        }, 500, 500);
+
+        final TextView timerText = findViewById(R.id.timetext);
+        timerText.setVisibility(View.VISIBLE);
+        gameTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(secondsLeft > 0){
+                    secondsLeft -= 1;
+                    timerText.setText(getString(R.string.timer) + secondsLeft);
+                } else {
+                    endGame();
+                }
+            }
+        }, 1000, 1000);
         start.setVisibility(View.INVISIBLE);
+    }
+
+    private void endGame(){
+        hideMoles();
+        posTimer.cancel();
+        gameTimer.cancel();
+
+        findViewById(R.id.timetext).setVisibility(View.INVISIBLE);
+
+        score = 0;
+        secondsLeft = 20;
+
+        //findViewById(R.id.start).setVisibility(View.VISIBLE);
     }
 
     private void changeMolePos() {
         changeMolePos(moles.get(rand.nextInt(moles.size())));
     }
 
-    private void changeMolePos(Button m) {
+    private void changeMolePos(final Button m) {
         RelativeLayout.LayoutParams p = (RelativeLayout.LayoutParams) m.getLayoutParams();
-        p.setMargins(rand.nextInt(findViewById(R.id.rl).getWidth()), rand.nextInt(findViewById(R.id.rl).getHeight()),0,0);
-        m.setLayoutParams(p);
+        p.setMargins(rand.nextInt(findViewById(R.id.rl).getWidth()-m.getWidth()), rand.nextInt(findViewById(R.id.rl).getHeight()-m.getHeight()),0,0);
+        final RelativeLayout.LayoutParams pfinal = p;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                m.setLayoutParams(pfinal);
+            }
+        });
+
     }
 
-
+    private void addScore(){
+        score++;
+        TextView start = findViewById(R.id.text);
+        start.setText(getString(R.string.score) + score);
+    }
 }
 
